5b73d98cc83f5eace4fe0ee1f3e8447d227c1f26
Hung Nguyen <nthungdev@gmail.com>
2024-11-09 17:11:01 -0500

    add flowbite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,27 @@
-import type { Config } from "tailwindcss";
-import colors from "tailwindcss/colors";
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
+import * as flowbite from 'flowbite-react/tailwind'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    flowbite.content(),
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
 
         primary: {
           ...colors.blue,
           DEFAULT: colors.blue[500],
-        }
+        },
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [flowbite.plugin()],
+}
+export default config
