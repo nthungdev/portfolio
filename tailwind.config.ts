67b7069be50955e4cde430dfa3f1912a33a6d1da
Hung Nguyen <nthungdev@gmail.com>
2024-11-09 14:27:15 -0500

    set primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const config: Config = {
   content: [
@@ -11,6 +12,11 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        primary: {
+          ...colors.blue,
+          DEFAULT: colors.blue[500],
+        }
       },
     },
   },
