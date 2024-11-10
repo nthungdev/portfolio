89c12b2609f58c80638ea8e9dfdf4c942a3721ad
Hung Nguyen <nthungdev@gmail.com>
2024-11-09 22:26:49 -0500

    update base section style

--- src/components/base-section.tsx
+++ src/components/base-section.tsx
@@ -2,8 +2,8 @@ import { ComponentProps } from 'react'
 
 export default function BaseSection(props: ComponentProps<'section'>) {
   return (
-    <section className="px-4 py-12 md:py-20 bg-gray-800" {...props}>
-      <div className="max-w-screen-lg mx-auto">
+    <section className="px-6 py-12 md:py-20 bg-gray-800" {...props}>
+      <div className="max-w-screen-xl mx-auto">
         {props.children}
       </div>
     </section>
