286de7688197c48d25e7ad725fc0c43d28109ec0
Hung Nguyen <nthungdev@gmail.com>
2024-11-11 23:00:18 -0500

    section title cetenter-able

--- src/components/section-title.tsx
+++ src/components/section-title.tsx
@@ -1,5 +1,21 @@
+import classNames from 'classnames'
 import { ReactNode } from 'react'
 
-export default function SectionTitle({ children }: { children: ReactNode }) {
-  return <h1 className="text-4xl md:text-6xl font-bold mb-8">{children}</h1>
+export default function SectionTitle({
+  children,
+  center,
+}: {
+  children: ReactNode
+  center?: boolean
+}) {
+  return (
+    <h1
+      className={classNames(
+        'text-4xl md:text-6xl font-bold mb-8',
+        center && 'text-center'
+      )}
+    >
+      {children}
+    </h1>
+  )
 }
