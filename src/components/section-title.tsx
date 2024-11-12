ed4e94dfbec79f40ea3dbda9316c87b9e9caab88
Hung Nguyen <nthungdev@gmail.com>
2024-11-11 22:29:18 -0500

    add margin to section title

--- src/components/section-title.tsx
+++ src/components/section-title.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react'
 
 export default function SectionTitle({ children }: { children: ReactNode }) {
-  return <h1 className="text-4xl md:text-6xl font-bold">{children}</h1>
+  return <h1 className="text-4xl md:text-6xl font-bold mb-8">{children}</h1>
 }
