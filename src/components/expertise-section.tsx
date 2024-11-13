af8d89539a2c9f52100f1751df69eb0d1987b8e1
Hung Nguyen <nthungdev@gmail.com>
2024-11-13 16:00:28 -0500

    animate expertises

--- src/components/expertise-section.tsx
+++ src/components/expertise-section.tsx
@@ -1,9 +1,12 @@
+'use client'
+
 import classNames from 'classnames'
-import BaseSection from './base-section'
-import SectionTitle from './section-title'
-import Chips from './chips'
 import expertise from '@/content/expertise'
 import IDS from '@/constants/ids'
+import ScrollElement from '@/components/scroll-element'
+import BaseSection from '@/components/base-section'
+import SectionTitle from '@/components/section-title'
+import Chips from '@/components/chips'
 
 const content = expertise
 
@@ -14,13 +17,18 @@ export default function ExpertiseSection() {
 
       <ul className="gap-16 mt-10 grid md:grid-cols-2">
         {content.expertise.map(
-          ({ icon: Icon, iconClasses, title, body, techStack }) => (
-            <li key={title} className="space-y-6">
-              <Icon className={classNames('size-12', iconClasses)} />
-              <h2 className="text-3xl md:text-4xl font-semibold">{title}</h2>
-              <p className="text-lg">{body}</p>
-              <Chips chips={techStack.map((name) => ({ name }))} />
-            </li>
+          ({ icon: Icon, iconClasses, title, body, techStack }, index) => (
+            <ScrollElement
+              key={title}
+              direction={index % 2 === 0 ? 'left' : 'right'}
+            >
+              <li className="space-y-6">
+                <Icon className={classNames('size-12', iconClasses)} />
+                <h2 className="text-3xl md:text-4xl font-semibold">{title}</h2>
+                <p className="text-lg">{body}</p>
+                <Chips chips={techStack.map((name) => ({ name }))} />
+              </li>
+            </ScrollElement>
           )
         )}
       </ul>
@@ -29,12 +37,17 @@ export default function ExpertiseSection() {
         {content.favoriteTechnologies.title}
       </h2>
       <div className="mt-10 flex flex-row justify-center md:justify-start">
-        <ul className="gap-4 md:gap-6 inline-grid grid-flow-row grid-cols-3 sm:flex flex-row flex-wrap">
+        <ul className="gap-4 md:gap-6 inline-grid grid-flow-row grid-cols-3 sm:flex flex-row flex-wrap overflow-hidden">
           {content.favoriteTechnologies.technologies.map(
             ({ icon: Icon, name }) => (
               <div
                 key={name}
                 className="group p-4 sm:p-4 size-28 sm:size-28 md:size-32 border space-y-2 bg-gray-900 hover:bg-gray-800 transition-colors"
+                // initial={{ translateX: 'calc(100vw)' }}
+                // whileInView={{
+                //   translateX: 0,
+                //   transition: { duration: 0.5, delay: 0.5 },
+                // }}
               >
                 <Icon className="size-12 md:size-16 mx-auto group-hover:scale-125 transition-transform" />
                 <div className="text-center text-xs md:text-base">{name}</div>
