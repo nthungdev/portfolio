0d4e15fc9654b8df74dacd952b17074c6bd13c13
Hung Nguyen <nthungdev@gmail.com>
2024-11-13 18:45:25 -0500

    update project card viewport

--- src/components/projects-section.tsx
+++ src/components/projects-section.tsx
@@ -12,12 +12,12 @@ export default function ProjectsSection() {
     <BaseSection id={IDS.projectsSection}>
       <SectionTitle>{content.title}</SectionTitle>
 
-      <div className="mt-10 px-2 grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 xl:gap-16">
+      <div className="mt-10 px-2 grid grid-cols-1 lg:grid-cols-2 gap-4 xl:gap-8">
         {content.projects.map((project, index) => (
           <ScrollElement
             key={index}
             direction={index % 2 === 0 ? 'left' : 'right'}
-            viewport={{ once: true, amount: 0.5, margin: '0px 0px 0px 0px' }}
+            viewport={{ once: false, amount: 0.3, margin: '50px 0px 50px 0px' }}
           >
             <ProjectCard project={project} />
           </ScrollElement>
