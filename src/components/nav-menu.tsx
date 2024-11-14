0eeeb2e25050e8029fb8f1f8816cdb19aaa7f41a
Hung Nguyen <nthungdev@gmail.com>
2024-11-13 19:13:09 -0500

    clean up log

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -42,7 +42,6 @@ export default function NavMenu() {
     }
 
     startTracking()
-    console.log({ top: element.offsetTop })
     window.scrollTo({
       top: element.offsetTop - scrollToOffset,
       behavior: 'smooth',
