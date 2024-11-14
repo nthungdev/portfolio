3699db69bfa5db0918914832dd367db727b3dee1
Hung Nguyen <nthungdev@gmail.com>
2024-11-13 20:23:16 -0500

    custom flowbite color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,31 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import { ThemeModeScript } from "flowbite-react";
-import Footer from "@/components/footer";
-import NavMenu from "@/components/nav-menu";
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.css'
+import { ThemeModeScript } from 'flowbite-react'
+import Footer from '@/components/footer'
+import NavMenu from '@/components/nav-menu'
+import CustomFlowbite from '@/components/custom-flowbite'
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+})
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+})
 
 export const metadata: Metadata = {
-  title: "Hung Nguyen",
-  description: "Hung Nguyen Portfolio",
-};
+  title: 'Hung Nguyen',
+  description: 'Hung Nguyen Portfolio',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
@@ -34,10 +35,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
-        <NavMenu />
-        {children}
-        <Footer />
+        <CustomFlowbite>
+          <NavMenu />
+          {children}
+          <Footer />
+        </CustomFlowbite>
       </body>
     </html>
-  );
+  )
 }
---
+++ src/components/custom-flowbite.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { CustomFlowbiteTheme, Flowbite } from 'flowbite-react'
+
+const customTheme: CustomFlowbiteTheme = {
+  textarea: {
+    colors: {
+      gray: 'border-gray-300 bg-gray-50 text-gray-900 focus:border-primary focus:ring-primary dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary dark:focus:ring-primary',
+      info: 'border-primary bg-primary-50 text-primary-900 placeholder-primary-700 focus:border-primary focus:ring-primary dark:border-primary-400 dark:bg-primary-100 dark:focus:border-primary dark:focus:ring-primary',
+      failure:
+        'border-red-500 bg-red-50 text-red-900 placeholder-red-700 focus:border-red-500 focus:ring-red-500 dark:border-red-400 dark:bg-red-100 dark:focus:border-red-500 dark:focus:ring-red-500',
+      warning:
+        'border-yellow-500 bg-yellow-50 text-yellow-900 placeholder-yellow-700 focus:border-yellow-500 focus:ring-yellow-500 dark:border-yellow-400 dark:bg-yellow-100 dark:focus:border-yellow-500 dark:focus:ring-yellow-500',
+      success:
+        'border-green-500 bg-green-50 text-green-900 placeholder-green-700 focus:border-green-500 focus:ring-green-500 dark:border-green-400 dark:bg-green-100 dark:focus:border-green-500 dark:focus:ring-green-500',
+    },
+  },
+  textInput: {
+    field: {
+      input: {
+        colors: {
+          gray: 'border-gray-300 bg-gray-50 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500',
+          info: 'border-primary-500 bg-primary-50 text-primary-900 placeholder-primary-700 focus:border-primary-500 focus:ring-primary-500 dark:border-primary-400 dark:bg-primary-100 dark:focus:border-primary-500 dark:focus:ring-primary-500',
+          failure:
+            'border-red-500 bg-red-50 text-red-900 placeholder-red-700 focus:border-red-500 focus:ring-red-500 dark:border-red-400 dark:bg-red-100 dark:focus:border-red-500 dark:focus:ring-red-500',
+          warning:
+            'border-yellow-500 bg-yellow-50 text-yellow-900 placeholder-yellow-700 focus:border-yellow-500 focus:ring-yellow-500 dark:border-yellow-400 dark:bg-yellow-100 dark:focus:border-yellow-500 dark:focus:ring-yellow-500',
+          success:
+            'border-green-500 bg-green-50 text-green-900 placeholder-green-700 focus:border-green-500 focus:ring-green-500 dark:border-green-400 dark:bg-green-100 dark:focus:border-green-500 dark:focus:ring-green-500',
+        },
+      },
+    },
+  },
+}
+
+export default function CustomFlowbite({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <Flowbite theme={{ theme: customTheme }}>{children}</Flowbite>
+}
